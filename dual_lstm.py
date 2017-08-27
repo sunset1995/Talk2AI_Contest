64389f930558c62f9c3b2516de05c360ad9944e0
Sunset <s2821d3721@gmail.com>
2017-08-27 22:32:26 +0800

    Fix Dual LSTM

--- dual_lstm.py
+++ dual_lstm.py
@@ -49,22 +49,23 @@ gc.collect()
 
 
 # reference: https://github.com/dennybritz/chatbot-retrieval/blob/8b1be4c2e63631b1180b97ef927dc2c1f7fe9bea/udc_hparams.py
-exp_name = 'dual_lstm_1'
+exp_name = 'dual_lstm_2'
 # Model Parameters
 params = {}
 save_params_dir = 'models/%s/' %exp_name
 params['word2vec_model_name'] = word2vec_fname
 params['word2vec_vocab_size'] = embedding_matrix.shape[0]
-params['word2vec_dim'] = embedding_matrix.shape[0]
-params['rnn_dim'] = 512  # 256, 384, 512
+params['word2vec_dim'] = embedding_matrix.shape[1]
+params['rnn_dim'] = 256  # 256, 384, 512
 params['n_layers'] = 1
 
 # Training Parameters
 params['learning_rate'] = 1e-4
 params['keep_prob_train'] = 0.8
 params['keep_prob_valid'] = 1.0
+params['l1_loss'] = 1e-4 # regularize M
 params['clip'] = 0.25
-params['batch_size'] = 512
+params['batch_size'] = 256
 params['eval_batch_size'] = 16
 params['n_iterations'] = int(40 * train_data_loader.data_num / params['batch_size'])
 
@@ -141,7 +142,8 @@ probs = tf.sigmoid(logits)
 correct_prediction = tf.logical_or( tf.logical_and(tf.equal(target,1), tf.greater_equal(probs,0.5)), tf.logical_and(tf.equal(target,0), tf.less(probs,0.5)))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 # Calculate the binary cross-entropy loss
-loss = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=tf.to_float(target)), name='mean_loss_of_batch')
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=tf.to_float(target)))
+loss = loss + params['l1_loss'] * tf.reduce_sum(tf.abs(M))
 
 #train_step = tf.train.AdamOptimizer(params['learning_rate']).minimize(loss)
 optimizer = tf.train.AdamOptimizer(params['learning_rate'])
@@ -202,69 +204,6 @@ with tf.Session() as sess:
         if not os.path.exists(record['newest_model_dir']):
             os.makedirs(record['newest_model_dir'])
         save_path = saver.save(sess, record['newest_model_dir']+'model.ckpt')
-
-
-            
-# Validation
-exp_name = 'dual_lstm_0'
-saver = tf.train.Saver()
-with tf.Session() as sess:
-    # Restore model
-    saver.restore(sess, record['best_model_dir']+'model.ckpt')
-    get_valid_loss_accuracy(sess)
-
-    
-
-sample = pd.read_csv('datas/sample_test_data.txt')
-sample_x1 = [[s for s in re.sub('[A-Z]:', '\t', _).split('\t') if len(s.strip())] for _ in sample.dialogue.values]
-sample_x2 = [[s for s in re.sub('[A-Z]:', '\t', _).split('\t') if len(s.strip())] for _ in sample.options.values]
-sample_y = sample.answer.values
-assert(np.sum([len(_)!=6 for _ in sample_x2]) == 0)
-sample_x1 = [[word for word in jieba.cut(' '.join(s)) if word != ' '] for s in sample_x1]
-sample_x2 = [[[word for word in jieba.cut(r) if word != ' '] for r in rs] for rs in sample_x2]
-
-test_datas = pd.read_csv('datas/AIFirstProblem.txt')
-test_x1 = [[s for s in re.sub('[A-Z]:', '\t', _).split('\t') if len(s.strip())] for _ in test_datas.dialogue.values]
-test_x2 = [[s for s in re.sub('[A-Z]:', '\t', _).split('\t') if len(s.strip())] for _ in test_datas.options.values]
-assert(np.sum([len(_)!=6 for _ in test_x2]) == 0)
-test_x1 = [[word for word in jieba.cut(' '.join(s)) if word != ' '] for s in test_x1]
-test_x2 = [[[word for word in jieba.cut(r) if word != ' '] for r in rs] for rs in test_x2]
-
-
-def word_lst_2_id_lst(lst):
-    pad_word_id = word2id['<pad>']
-    return [word2id[lst[i]] if i<len(lst) and lst[i] in word2id else pad_word_id for i in range(max_seq_len)]
-
-sample_id1 = np.array([word_lst_2_id_lst(s) for s in sample_x1])
-sample_id2 = np.array([[word_lst_2_id_lst(r) for r in rs] for rs in sample_x2])
-test_id1 = np.array([word_lst_2_id_lst(s) for s in test_x1])
-test_id2 = np.array([[word_lst_2_id_lst(r) for r in rs] for rs in test_x2])
-
-
-def eval_ans(lst_id1, lst_id2):
-    score = sess.run(logits, {
-        context: np.repeat(lst_id1, 6, axis=0),
-        response: lst_id2.reshape(-1, max_seq_len),
-        keep_prob: params['keep_prob_valid']})
-    score = score.reshape(-1, 6)
-    return np.argmax(score, axis=1)
-
-def eval_on_sample():
-    my_ans = eval_ans(sample_id1, sample_id2)
-    return np.sum(my_ans == sample_y)
-
-
-# Predict on validation dataset
-saver = tf.train.Saver()
-with tf.Session() as sess:
-    # Restore model
-    saver.restore(sess, record['best_model_dir']+'model.ckpt')
-    get_valid_loss_accuracy(sess)
-    sample_correct = eval_on_sample()
-    my_sample_ans = eval_ans(sample_id1, sample_id2)
-    my_test_ans = eval_ans(test_id1, test_id2)
-    print('sample correct %4d' % (sample_correct), flush=True)
-    record['sample_correct'] = sample_correct.tolist()
 
 
 if not os.path.exists(save_record_dir):
