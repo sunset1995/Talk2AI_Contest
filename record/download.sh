d46ae891586f36428aa9316e48f5b94144ba2b0a
Sunset <s2821d3721@gmail.com>
2017-09-21 00:05:57 +0800

    Track exp_20

--- record/download.sh
+++ record/download.sh
@@ -7,6 +7,7 @@ scp -i ~/.ssh/google-cloud s2821d3721@35.199.165.200:~/Talk2AI_Contest/tmp_* ./
 scp -i ~/.ssh/google-cloud s2821d3721@104.198.217.161:~/Talk2AI_Contest/tmp_* ./
 scp -i ~/.ssh/google-cloud s2821d3721@35.196.130.216:~/Talk2AI_Contest/tmp_* ./
 scp aiuser3603@140.110.9.19:~/tmp_dual_lstm_18 ./
+scp aiuser3603@140.110.9.19:~/tmp_dual_20 ./
 
 cat tmp_dual_lstm_12 | grep Valid > exp_12
 cat tmp_dual_lstm_13 | grep Valid > exp_13
@@ -15,3 +16,4 @@ cat tmp_dual_lstm_15 | grep Valid > exp_15
 cat tmp_dual_lstm_16 | grep Valid > exp_16
 cat tmp_dual_lstm_17 | grep Valid > exp_17
 cat tmp_dual_lstm_18 | grep Valid > exp_18
+cat tmp_dual_20 | grep Valid > exp_20
