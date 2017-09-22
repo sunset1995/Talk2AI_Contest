0de0bb8cd031af5bc38eb42de22f41f77a8eeaea
Sunset <s2821d3721@gmail.com>
2017-09-22 09:57:42 +0800

    Update downloader

--- record/download.sh
+++ record/download.sh
@@ -8,6 +8,8 @@ scp -i ~/.ssh/google-cloud s2821d3721@104.198.217.161:~/Talk2AI_Contest/tmp_* ./
 scp -i ~/.ssh/google-cloud s2821d3721@35.196.130.216:~/Talk2AI_Contest/tmp_* ./
 scp aiuser3603@140.110.9.19:~/tmp_dual_lstm_18 ./
 scp aiuser3603@140.110.9.19:~/tmp_dual_20 ./
+scp aiuser3603@140.110.9.19:~/tmp_dual_22 ./
+scp aiuser3603@140.110.9.19:~/tmp_dual_23 ./
 
 cat tmp_dual_lstm_12 | grep Valid > exp_12
 cat tmp_dual_lstm_13 | grep Valid > exp_13
@@ -17,3 +19,5 @@ cat tmp_dual_lstm_16 | grep Valid > exp_16
 cat tmp_dual_lstm_17 | grep Valid > exp_17
 cat tmp_dual_lstm_18 | grep Valid > exp_18
 cat tmp_dual_20 | grep Valid > exp_20
+cat tmp_dual_22 | grep Valid > exp_22
+cat tmp_dual_23 | grep Valid > exp_23
